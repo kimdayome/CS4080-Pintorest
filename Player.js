e7b6442a88d4893500d2b95a9866585f51895d2c
dayoung kim <dayoung7406@gmail.com>
2024-11-12 12:00:26 -0800

    all component files

---
+++ Player.js
@@ -0,0 +1,23 @@
+// Player.js
+
+import React from "react";
+import './Player.css';
+
+function Player() {
+    return (
+        <div className="player">
+        <div className="player-main">
+          <div className="info">
+            <div className="current">current</div>
+            <div className="title">Dayoung</div>
+            <div className="artist">Jessica</div>
+          </div>
+          <div className="time">
+            2:34 / 5:67
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Player;
